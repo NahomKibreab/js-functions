198340f21785710766727b73c657a0f3ad5fe91c
NahomKibreab <nknahom@gmail.com>
2023-09-16 11:52:39 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// // functional expression
+// function name(arguments) {
+//   return;
+// }
+
+// // anonymous function
+// function () {
+// return
+// }
+
+// // arrow function
+// let name = () => {
+//   return
+// }
